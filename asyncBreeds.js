ae6c94cd23e27c0fc4200944c9bb4e94e476d674
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-02-12 01:47:09 +0000

    fixed bug

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -1,23 +1,27 @@
 const fs = require('fs');
 
-const breedDetailsFromFile = function(breed, callback) {
-  //console.log('breedDetailsFromFile: Calling readFile...');
+
+const breedDetailsFromFile = function(breed, printCatBreed) {
+  console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
     console.log("In readFile's Callback: it has the data.");
     if (!error) {
-        callback(data);
+      printCatBreed(data);
     } else {
-       callback(undefined); 
-    }    
+      printCatBreed(undefined);
+    }
   });
-
 };
 
-const printOutCatBreed = breed => {
-    console.log('Return Value: ', data)
+const printCatBreed = (breed) => {
+  console.log('Return Value: ', breed);
 };
 
-//breedDetailsFromFile('Bombay', printOutCatBreed);
+// we try to get the return value
+const bombay = breedDetailsFromFile('Bombay', printCatBreed);
+// console.log('Return Value: ', bombay); 
+
+
 
 
 module.exports = breedDetailsFromFile
